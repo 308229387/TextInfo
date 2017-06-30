f29c45233faeb908fe009675650a62611f1653a0
songyongmeng <songyongmeng@ainirobot.com>
2017-06-30 11:48:15 +0800

    重新更换显示方式

--- app/src/main/java/com/textinfo/MainActivity.java
+++ app/src/main/java/com/textinfo/MainActivity.java
@@ -1,7 +1,12 @@
 package com.textinfo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.text)).setText(String.format(getString(R.string.app_text,"3","4")));
+
+        TextView textView = (TextView)findViewById(R.id.text);
+
+        String text = String.format(getResources().getString(R.string.baoxiang), 2,18,"银宝箱");
+        int index[] = new int[3];
+        index[0] = text.indexOf("2");
+        index[1] = text.indexOf("18");
+        index[2] = text.indexOf("银宝箱");
+
+        SpannableStringBuilder style=new SpannableStringBuilder(text);
+        style.setSpan(new ForegroundColorSpan(Color.RED),index[0],index[0]+1, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);//ContextCompat.getColor(getActivity(), R.color.main_theme_color)
+        style.setSpan(new ForegroundColorSpan(Color.RED),index[1],index[1]+2,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        style.setSpan(new BackgroundColorSpan(Color.RED),index[2],index[2]+3,Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        textView.setText(style);
+
     }
 }
